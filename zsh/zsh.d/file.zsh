23b005ab1fa19c4b08e92256a25e7f4f5d0bc609
duyhenryer <duyhenry250897@gmail.com>
2019-05-02 18:39:22 +0700

    commit #zsh

--- zsh/zsh.d/file.zsh
+++
@@ -1,7 +0,0 @@
-#!/bin/zsh
-
-# Alias for search functions
-
-alias fd='find . -type d -name'
-alias ff='find . -type f -name'
-
--- zsh/zsh.d/prompt.d/root.zsh
+++
@@ -1,4 +0,0 @@
-# Based on robbyrussell's theme, with host and rvm indicators. Example:
-# @host ➜ currentdir rvm:(rubyversion@gemset) git:(branchname)
-
-PROMPT="%{$fg_bold[red]%}@$HOST[0,4] ➜  %{$fg_bold[cyan]%}%c "
